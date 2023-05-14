0f37ddcc7b68c75b38a559f95760ed96f88810c6
Ian Brock <pibborando@gmail.com>
2023-05-14 09:15:47 -0700

    ✨ Add "carry" property and improve visual feedback on sorting

--- src/App.js
+++ src/App.js
@@ -10,29 +10,32 @@ class App extends Component {
     'sort_prop': 'character',
     'reverse_sort': false
   }
-  sort_by = (sort_prop) => {
-    if (sort_prop === this.state.sort_prop) {
-      this.setState({'reverse_sort': !this.state.reverse_sort})
-      return
-    }
-    this.setState({'sort_prop': sort_prop, 'reverse_sort': false})
+  sort_by(sort_prop) {
+    let reverse_sort = (sort_prop === this.state.sort_prop)? !this.state.reverse_sort: false
+    this.setState({'sort_prop': sort_prop, 'reverse_sort': reverse_sort})
+  }
+  sort_status(prop) {
+    if (prop !== this.state.sort_prop) return ''
+    if (this.state.reverse_sort) return 'descend'
+    return 'ascend'
   }
   render() {
     this.state.combo_data.sort((a, b) => (
       compare(a[this.state.sort_prop], b[this.state.sort_prop], this.state.reverse_sort)
     ))
     const props_list = combos.props.map(prop => (
-      <div className='prop-name' onClick={(e) => this.sort_by(e.target.textContent)}>
+      <div className={'prop-header '+(this.sort_status(prop)? 'sorted':'')}
+          data-sort={this.sort_status(prop)} onClick={(e) => this.sort_by(e.target.textContent)}>
         {prop}
       </div>
     ))
     const combo_list = this.state.combo_data.map(combo => (
       combos.props.map(prop => (
-        <div className='prop-value'>
+        <div className={'prop-value '+(this.sort_status(prop)? 'sorted':'')}
+            data-status={(combo[prop] >= 0 && prop === 'advantage')?'+':''}>
           {combo[prop]}
         </div>
-        )
-      )
+      ))
     ))
     return (
       <div className='app'>
