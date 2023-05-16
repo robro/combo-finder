cee812af6e6feff77822fab37d91b375a5b93e0d
Ian Brock <pibborando@gmail.com>
2023-05-15 20:44:25 -0700

    ✨ Make page buttons and input work

--- src/App.js
+++ src/App.js
@@ -16,8 +16,45 @@ function App() {
   const [page_size, setPageSize] = useState(20)
   const [cur_page, setCurPage] = useState(1)
 
-  const setSorting = (event) => {
-    const new_prop = event.target.textContent
+  const pageUp = () => {
+    if (cur_page >= total_pages) return
+    let page_input = document.getElementById('page-input')
+    page_input.value = cur_page+1
+    setCurPage(cur_page+1)
+  }
+  const pageDown = () => {
+    if (cur_page <= 1) return
+    let page_input = document.getElementById('page-input')
+    page_input.value = cur_page-1
+    setCurPage(cur_page-1)
+  }
+  const goToPage = () => {
+    let page_input = document.getElementById('page-input')
+    if (page_input.value === '') {
+      page_input.value = cur_page
+      return
+    }
+    let page_input_num = Number(page_input.value)
+    if (page_input_num === cur_page) return
+    if (page_input_num < 1) page_input_num = 1
+    if (page_input_num > total_pages) page_input_num = total_pages
+    page_input.value = page_input_num
+    setCurPage(page_input_num)
+  }
+  const getDownStatus = () => {
+    if (cur_page === 1) return 'disabled'
+    return ''
+  }
+  const getUpStatus = () => {
+    if (cur_page === total_pages) return 'disabled'
+    return ''
+  }
+  const getGoStatus = () => {
+    if (total_pages === 1) return 'disabled'
+    return ''
+  }
+  const setSorting = (e) => {
+    const new_prop = e.target.textContent
     const new_sort = (new_prop === sort_prop)? !reverse_sort: false
     setSortProp(new_prop)
     setReverseSort(new_sort)
@@ -27,15 +64,15 @@ function App() {
     if (reverse_sort) return 'descend'
     return 'ascend'
   }
-  const updateFilters = (event) => {
-    const key = event.target.name
-    const op = event.target.id
-    const value = event.target.value
+  const updateFilters = (e) => {
+    const key = e.target.name
+    const op = e.target.id
+    const value = e.target.value
     const new_filters = {...filters, [key]: {'op': op, 'value': value}}
     if (!value) delete new_filters[key]
     setFilters(new_filters)
   }
-  const resetFilters = (event) => {
+  const resetFilters = () => {
     let dropdown = document.getElementById('is')
     dropdown.value = ''
     setFilters({})
@@ -45,12 +82,19 @@ function App() {
       if (filters[prop].value) {
         if (!evaluate[filters[prop].op](combo[prop], filters[prop].value)) {
           return false
-    }}}
+        }}}
     return true
   }
   const filtered_data = combo_data.filter(filterCombo).sort((a, b) => (
     compare(a[sort_prop], b[sort_prop], reverse_sort)
   ))
+  const total_pages = Math.max(1, Math.ceil(filtered_data.length / page_size))
+  if (cur_page > total_pages) {
+    let page_input = document.getElementById('page-input')
+    page_input.value = total_pages
+    setCurPage(total_pages)
+  }
+
   const getInfoString = () => {
     let filter_info = ''
     let combo_nums = ''
@@ -83,8 +127,9 @@ function App() {
       ))}
     </tr>
   ))
-  const reset_filters = (Object.keys(filters).length > 0)? 
+  const reset_filters = (Object.keys(filters).length > 0)?
     <span className='reset' onClick={resetFilters}>Reset filters</span>: ''
+
   return (
     <div className='app'>
       <div className='title-bar'>
@@ -106,10 +151,11 @@ function App() {
           </table>
         </div>
         <div className='page-select'>
-          <button className='btn btn-left' type='button'>🞀</button>
-          <button className='btn btn-right shift' type='button'>🞂</button>
-          <input className='page-input' type='number' min={1} max={10} defaultValue={1}/>
-          <button className='btn btn-right shift' type='button'>Go</button>
+          <button className={'btn btn-left'} type='button' onClick={pageDown} disabled={getDownStatus()}>🞀</button>
+          <button className={'btn btn-right shift'} type='button' onClick={pageUp} disabled={getUpStatus()} >🞂</button>
+          <input className='page-input' id='page-input' type='number' required
+            min='1' max={total_pages} defaultValue={cur_page} disabled={getGoStatus()}/>
+          <button className='btn btn-right shift' type='button' onClick={goToPage} disabled={getGoStatus()}>Go</button>
         </div>
         <hr className='solid'/>
       </div>
