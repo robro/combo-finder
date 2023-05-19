9d93e4dc8113a6f1180d9446b3d3196363568b8b
Ian Brock <pibborando@gmail.com>
2023-05-19 12:04:18 -0700

    ✨ Add "starts with" and "ends with" comparison to notation filter

--- src/evaluate.js
+++ src/evaluate.js
@@ -4,6 +4,8 @@ const evaluate = {
     'less than': function(a, b) { return a < b },
     'contains': function(a, b) { return a.includes(b) },
     'doesn\'t contain': function(a, b) { return !a.includes(b) },
+    'starts with': function(a, b) { return a.startsWith(b) },
+    'ends with': function(a, b) { return a.endsWith(b) },
 }
 
 export default evaluate
