7b6f2cdebe9c0a199b3cb4713e3c15bf7cac4ac8
Ian Brock <pibborando@gmail.com>
2023-05-19 01:12:31 -0700

    📝 Start adding real combos

---
+++ addcombo.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+import json
+
+file_name = './src/combos.json'
+
+def main():
+  with open(file_name, 'r') as combos_file:
+    combo_str = combos_file.read()
+    combo_json: list = json.loads(combo_str)
+
+  combo_json.append({
+    'character': input('Character: '),
+    'notation': input('Notation: '),
+    'startup': int(input('Startup: ')),
+    'drive': float(input('Drive: ')),
+    'super': int(input('Super: ')),
+    'carry': int(input('Carry: ')),
+    'damage': int(input('Damage: ')),
+    'advantage': int(input('Advantage: ')),
+    'position': input('Position:')
+  })
+
+  with open(file_name, 'w') as combos_file:
+    combos_file.write(json.dumps(combo_json, indent=2))
+
+if __name__ == "__main__":
+  main()
