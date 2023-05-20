0a617c1a633884fd34d5d7b9b994efedded6f6af
Ian Brock <pibborando@gmail.com>
2023-05-19 20:55:29 -0700

    🔧 Don't capitalize notation text

--- src/Footer.js
+++ src/Footer.js
@@ -4,26 +4,28 @@ export default function Footer() {
   return (
     <div className='footer'>
       <table className='full-width'>
-        <tr>
-          <td className='align-top'>
-            Need help? Check the <NotationGuide>combo notation guide.</NotationGuide>
-          </td>
-          <td className='align-right'>
-            Have a suggestion? <a
-              href='https://twitter.com/robrotic'
-              target="_blank"
-              rel="noopener noreferrer">
-              Contact me on Twitter.
-            </a>
-            <br></br>
-            Or check out the <a
-              href='https://github.com/robro/combo-finder'
-              target="_blank"
-              rel="noopener noreferrer">
-              repository.
-            </a>
-          </td>
-        </tr>
+        <tbody>
+          <tr>
+            <td className='align-top'>
+              Need help? Check the <NotationGuide>combo notation guide.</NotationGuide>
+            </td>
+            <td className='align-right'>
+              Have a suggestion? <a
+                href='https://twitter.com/robrotic'
+                target="_blank"
+                rel="noopener noreferrer">
+                Contact me on Twitter.
+              </a>
+              <br></br>
+              Or check out the <a
+                href='https://github.com/robro/combo-finder'
+                target="_blank"
+                rel="noopener noreferrer">
+                repository.
+              </a>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
