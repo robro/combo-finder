382eeecc38d9454d99bb6d1da9e088ea9701b9d9
robro <pibborando@gmail.com>
2023-05-20 00:23:32 -0700

    ✨Add some more Jamie combos

--- addcombo.py
+++ addcombo.py
@@ -5,24 +5,27 @@ import json
 file_name = './src/combos.json'
 
 def main():
-  with open(file_name, 'r') as combos_file:
-    combo_str = combos_file.read()
-    combo_json: list = json.loads(combo_str)
+  while True:
+    with open(file_name, 'r') as combos_file:
+      combo_str = combos_file.read()
+      combo_json: list = json.loads(combo_str)
 
-  combo_json.append({
-    'character': input('Character: '),
-    'notation': input('Notation: '),
-    'startup': int(input('Startup: ')),
-    'drive': float(input('Drive: ')),
-    'super': int(input('Super: ')),
-    'carry': int(input('Carry: ')),
-    'damage': int(input('Damage: ')),
-    'advantage': int(input('Advantage: ')),
-    'position': input('Position: ')
-  })
+    combo_json.append({
+      'character': input('Character: '),
+      'notation': input('Notation: '),
+      'startup': int(input('Startup: ')),
+      'drive': float(input('Drive: ')),
+      'super': int(input('Super: ')),
+      'carry': int(input('Carry: ')),
+      'damage': int(input('Damage: ')),
+      'advantage': int(input('Advantage: ')),
+      'position': input('Position: ')
+    })
 
-  with open(file_name, 'w') as combos_file:
-    combos_file.write(json.dumps(combo_json, indent=2))
+    with open(file_name, 'w') as combos_file:
+      combos_file.write(json.dumps(combo_json, indent=2))
+
+    print('New combo added!\n')
 
 if __name__ == "__main__":
   main()
