f69e77b4f886c202b0ad78aed2acfec50d878948
Ian Brock <pibborando@gmail.com>
2023-05-29 15:26:27 -0700

    🔧 Add duplicate protection and updating to addcombo.py

--- addcombo.py
+++ addcombo.py
@@ -10,7 +10,7 @@ def main():
       combo_str = combos_file.read()
       combo_json: list = json.loads(combo_str)
 
-    combo_json.append({
+    new_combo = {
       'character': input('Character: '),
       'notation': input('Notation: '),
       'startup': int(input('Startup: ')),
@@ -21,12 +21,24 @@ def main():
       'advantage': int(input('Advantage: ')),
       'position': input('Position: '),
       'state': input('State: '),
-    })
+    }
+
+    for i, combo in enumerate(combo_json):
+      if (combo['character'] == new_combo['character']
+          and combo['notation'] == new_combo['notation']):
+        replace = ''
+        while replace.lower() not in ['y', 'n']:
+          replace = input(f'Replace existing {combo["character"]} combo with same notation? Y/N: ')
+          if replace.lower() == 'y':
+            combo_json[i] = new_combo
+        break
+    else:
+      combo_json.append(new_combo)
 
     with open(file_name, 'w') as combos_file:
       combos_file.write(json.dumps(combo_json, indent=2))
 
-    print('New combo added!\n')
+    print('Finished!\n')
 
 if __name__ == "__main__":
   main()
